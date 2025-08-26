42fb7b94b0b64c4dd90951eef4d6ba9b8798d9ad
Fudi <fudi.mahfudin@gmail.com>
2025-08-26 11:33:42 +0700

    feat: Set up auto routes unplugin-vue-router

---
+++ src/pages/[...catchAll].vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import { RouterLink } from 'vue-router'
+</script>
+
+<template>
+  <div>
+    <h1>Not found</h1>
+    <RouterLink to="/">Home</RouterLink>
+  </div>
+</template>
---
+++ src/pages/projects/index.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import { RouterLink } from 'vue-router'
+</script>
+
+<template>
+  <div>
+    <h1>Projects</h1>
+    <RouterLink to="/">Home</RouterLink>
+  </div>
+</template>
