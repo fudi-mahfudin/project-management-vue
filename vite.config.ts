f91392b0e307289b542d6db8a0cf6631f78ea7b7
Fudi <fudi.mahfudin@gmail.com>
2025-08-30 18:24:49 +0700

    feat: setup Shadcn-vue

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,11 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 import vueRouter from 'unplugin-vue-router/vite'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vueRouter(), vue(), vueDevTools()],
+  plugins: [vueRouter(), vue(), vueDevTools(), tailwindcss()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
